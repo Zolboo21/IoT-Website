21ea86e03f26b386dc51f294ca08e81085a13c02
zolboo21 <zolboo.oz@gmail.com>
2022-11-29 02:40:53 +0900

    could not figure out mongodb part
    보내기를 누르면 form안에서 add으로 이동하지 않음

--- index.js
+++ index.js
@@ -13,19 +13,20 @@ const MongoClient = require('mongodb').MongoClient
 MongoClient.connect("mongodb+srv://zolboo21:hallym@cluster0.dfimkyo.mongodb.net/?retryWrites=true&w=majority", function(err, client){
     if(err) return console.log(err)
     db = client.db('nodejs');
-    db.collection('login').insertOne({email: "asd@gmail.com"},
-        console.log("sace complete")
-    )
-
-
-
-
-
+    
     app.listen(5500, function(){
         console.log('listening on 5500')
     })
 })
-
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html')
-})
+    app.get('/', function(req, res){
+        res.sendFile(__dirname + '/index.html')
+        
+    })
+    app.post('/add', function(req, res){
+        res.send("complete..")
+        db.collection('login').insertOne({email:req.body.email}, function(err, result){
+        console.log("save complete")
+        console.log(req.body.email);
+        })
+        
+    })
