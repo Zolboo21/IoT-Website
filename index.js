0545ab71b944c99be38209ec42fd53723937edf0
zolboo21 <zolboo.oz@gmail.com>
2022-11-28 22:53:55 +0900

    mongodb test

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 // express을 사용하기
 const express = require('express');
+//const { MongoClient } = require('mongodb');
 const app = express();
 
-app.listen(5500, function(){
-    console.log('listening on 8080')
+// app.listen(5500, function(){
+//     console.log('listening on 5500')
+// })
+var db;
+
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect("mongodb+srv://zolboo21:hallym@cluster0.dfimkyo.mongodb.net/?retryWrites=true&w=majority", function(err, client){
+    if(err) return console.log(err)
+    db = client.db('nodejs');
+    db.collection('login').insertOne({email: "asd@gmail.com"},
+        console.log("sace complete")
+    )
+
+
+
+
+
+    app.listen(5500, function(){
+        console.log('listening on 5500')
+    })
 })
 
 app.get('/', function(req, res){
