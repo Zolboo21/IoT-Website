3c6dcbb6d2fdef41bc97d614f192787b4988e3cd
zolboo21 <zolboo.oz@gmail.com>
2022-12-04 22:25:45 +0900

    final edits

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-// express을 사용하기
 const express = require('express');
-//const { MongoClient } = require('mongodb');
 const app = express();
-
-// app.listen(5500, function(){
-//     console.log('listening on 5500')
-// })
+app.use(express.urlencoded({extended: true})) 
+const bodyParser= require('body-parser')
+app.use(bodyParser.urlencoded({extended: true})) 
 var db;
-
 const MongoClient = require('mongodb').MongoClient
+app.set('view engine', 'ejs');
+const alert = console.log.bind(console);
+
+
+
 
 MongoClient.connect("mongodb+srv://zolboo21:hallym@cluster0.dfimkyo.mongodb.net/?retryWrites=true&w=majority", function(err, client){
     if(err) return console.log(err)
     db = client.db('nodejs');
+
+    console.log('DB connected')
     
     app.listen(5500, function(){
         console.log('listening on 5500')
@@ -22,20 +25,46 @@ MongoClient.connect("mongodb+srv://zolboo21:hallym@cluster0.dfimkyo.mongodb.net/
         res.sendFile(__dirname + '/index.html')
         
     })
-    app.post('/submit', function(req, res){
-        res.send("complete..")
-        db.collection('submit').insertOne({email:req.body.Pemail}, function(err, result){
-        console.log("save complete")
-        console.log(req.body.Pemail);
+
+
+
+    app.get('/list', function(req, res) {
+        db.collection('login').find().toArray(function(err, result){
+          console.log(result);
+          res.render('list.ejs', {loginfo : result})
         })
-        
-        
-    })
-    app.post('/add', function(req, res){
-        res.send("complete..")
-        db.collection('login').insertOne({email:req.body.email}, function(err, result){
-        console.log("save complete")
-        console.log(req.body.email);
-        })
-        
-    })
+      })
+
+      app.post('/add', function(req, res){
+        db.collection('config').findOne({name: 'totalcount'}, function(err, result){
+          var mycount = result.count;
+          db.collection('login').insertOne( { _id : (mycount +1), email : req.body.Pemail} , function(){
+            db.collection('config').updateOne({name: 'totalcount'},{ $inc:{count:1} },function(err, result){
+              if(err) return console.log(err)
+              res.sendFile(__dirname +'/alert.html')
+            })  
+          });
+        });
+      });
+      app.post('/sub', function(req, res){
+        db.collection('config').findOne({name: 'totalcount2'}, function(err, result){
+          var mycount2 = result.count2;
+          db.collection('sign').insertOne( { _id : (mycount2 +1), name : req.body.FirstName, school : req.body.School, FirstEmail : req.body.FirstEmail, PhoneNumber : req.body.PhoneNumber} , function(){
+            db.collection('config').updateOne({name: 'totalcount2'},{ $inc:{count2:1} },function(err, result){
+              if(err) return console.log(err)
+              res.sendFile(__dirname +'/alert.html')
+            })  
+          });
+        });
+      });
+      app.post('/zoom', function(req, res){
+        db.collection('config').findOne({name: 'totalcount3'}, function(err, result){
+          var mycount3 = result.count3;
+          db.collection('zoom').insertOne( { _id : (mycount3 +1), name : req.body.zoomname, email : req.body.Pemail} , function(){
+            db.collection('config').updateOne({name: 'totalcount3'},{ $inc:{count:1} },function(err, result){
+              if(err) return console.log(err)
+              res.sendFile(__dirname +'/alert.html')
+            })  
+          });
+        });
+      });
